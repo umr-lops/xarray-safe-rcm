1d85bffda83d7c121884f809fd189a418b7d84b3
Justus Magin <keewis@posteo.de>
2023-02-23 16:45:06 +0100

    add a utility function to split out dict keys starting with a `@`

--- safe_rcm/product/utils.py
+++ safe_rcm/product/utils.py
@@ -1,6 +1,16 @@
 import toolz
 
 
+def split_attributes(mapping):
+    groups = toolz.itertoolz.groupby(
+        lambda item: item[0].startswith("@"), mapping.items()
+    )
+    attrs = {key.lstrip("@"): value for key, value in groups[True]}
+    data = {key: value for key, value in groups[False]}
+
+    return attrs, data
+
+
 def strip_namespaces(name, namespaces):
     """remove the given namespaces from a name
 
