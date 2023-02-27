8c05fe5b79581ae40780abcc6a21cf22f531f8b8
Justus Magin <keewis@posteo.de>
2023-02-27 17:38:33 +0100

    refactor the url utils to always check if the url is nested

--- safe_rcm/fs_utils.py
+++ safe_rcm/fs_utils.py
@@ -2,6 +2,15 @@ import posixpath
 from urllib.parse import urlsplit, urlunsplit
 
 
+def split_url(url):
+    if url.count("::") > 1:
+        # TODO: unlike urllib.parse, `fsspec` allows nested urls
+        #       so we need to find a way to support that, as well
+        raise ValueError("don't know how to deal with nested urls")
+
+    return urlsplit(url)
+
+
 def normalize_url_path(url):
     """convert the url's path component to absolute"""
     if url.count("::") > 1:
@@ -16,13 +25,13 @@ def normalize_url_path(url):
 
 
 def dirname(url):
-    split = urlsplit(url)
+    split = split_url(url)
     new_path = posixpath.dirname(split.path)
     return urlunsplit(split._replace(path=new_path))
 
 
 def join_path(url, path):
-    split = urlsplit(url)
+    split = split_url(url)
     if posixpath.isabs(path):
         joined_path = path
     else:
