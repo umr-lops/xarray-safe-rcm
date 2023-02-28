55629e0f9996842cfd3685e27b647fdc4cc990a5
Justus Magin <keewis@posteo.de>
2023-02-28 10:33:46 +0100

    use `split_url` to verify we can deal with the url

--- safe_rcm/fs_utils.py
+++ safe_rcm/fs_utils.py
@@ -13,12 +13,7 @@ def split_url(url):
 
 def normalize_url_path(url):
     """convert the url's path component to absolute"""
-    if url.count("::") > 1:
-        # TODO: unlike urllib.parse, `fsspec` allows nested urls
-        #       so we need to find a way to support that, as well
-        raise ValueError("don't know how to deal with nested urls")
-
-    split = urlsplit(url)
+    split = split_url(url)
     normalized = posixpath.normpath(split.path)
 
     return urlunsplit(split._replace(path=normalized))
