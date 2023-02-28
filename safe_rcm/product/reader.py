24e85e3b9f2c5d455ea589a6f1028d54a9c28f02
Justus Magin <keewis@posteo.de>
2023-02-28 17:44:22 +0100

    don't use the fully-qualified name for the `functoolz` functions

--- safe_rcm/product/reader.py
+++ safe_rcm/product/reader.py
@@ -1,20 +1,21 @@
 import datatree
 import toolz
 import xarray as xr
+from toolz.functoolz import compose_left, curry
 
 from ..xml import read_xml
 from . import converters, transformers
 from .dicttoolz import query
 
 
-@toolz.functoolz.curry
+@curry
 def execute(mapping, f, path):
     subset = query(path, mapping)
 
     return f(subset)
 
 
-@toolz.functoolz.curry
+@curry
 def convert(converters, item):
     key, value = item
     converter = converters.get(key, lambda x: x)
@@ -27,9 +28,7 @@ def read_product(fs, product_url):
     layout = {
         "/": {
             "path": "/",
-            "f": toolz.functoolz.curry(converters.extract_metadata)(
-                collapse=["securityAttributes"]
-            ),
+            "f": curry(converters.extract_metadata)(collapse=["securityAttributes"]),
         },
         "/imageReferenceAttributes": {
             "path": "/imageReferenceAttributes",
@@ -37,14 +36,12 @@ def read_product(fs, product_url):
         },
         "/geographicInformation/ellipsoidParameters": {
             "path": "/imageReferenceAttributes/geographicInformation/ellipsoidParameters",
-            "f": toolz.functoolz.curry(transformers.extract_dataset)(dims="params"),
+            "f": curry(transformers.extract_dataset)(dims="params"),
         },
         "/geographicInformation/geolocationGrid": {
             "path": "/imageReferenceAttributes/geographicInformation/geolocationGrid/imageTiePoint",
-            "f": toolz.functoolz.compose_left(
-                toolz.functoolz.curry(transformers.extract_nested_datatree)(
-                    dims="tie_points"
-                ),
+            "f": compose_left(
+                curry(transformers.extract_nested_datatree)(dims="tie_points"),
                 lambda tree: xr.merge([node.ds for node in tree.subtree]),
                 lambda ds: ds.set_index(tie_points=["line", "pixel"]),
                 lambda ds: ds.unstack("tie_points"),
