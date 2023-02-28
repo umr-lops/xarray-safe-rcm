be3cbb9251dfe3fb85486e7c4ffe091dfb86e5b7
Justus Magin <keewis@posteo.de>
2023-02-28 16:56:09 +0100

    add a predicate to determine whether an object describes an array

--- safe_rcm/product/predicates.py
+++ safe_rcm/product/predicates.py
@@ -13,3 +13,32 @@ def is_complex(obj):
         return False
 
     return [el["@dataStream"].lower() for el in obj] == ["real", "imaginary"]
+
+
+def is_array(obj):
+    # definition of a array:
+    # - list of scalars
+    # - list of 1d lists
+    # - complex array:
+    #   - complex parts
+    #   - list of complex values
+    if not isinstance(obj, list):
+        return False
+
+    if len(obj) == 0:
+        # zero-sized list, not sure what to do here
+        return False
+
+    elem = obj[0]
+    if is_complex(obj):
+        return not is_scalar(elem["$"])
+    elif is_scalar(elem):
+        return True
+    elif isinstance(elem, list):
+        if len(elem) == 1 and is_scalar(elem[0]):
+            return True
+        elif is_complex(elem):
+            # array of imaginary values
+            return True
+
+    return False
