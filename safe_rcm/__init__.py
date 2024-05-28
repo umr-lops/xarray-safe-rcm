32f8b232c79336a71edcdca1ee37890876bb058b
agrouaze <antoine.grouazel@gmail.com>
2024-05-28 16:48:43 +0200

    forget about the old python syntaxe, simply fix the package name

--- safe_rcm/__init__.py
+++ safe_rcm/__init__.py
@@ -1,7 +1,8 @@
+from importlib.metadata import version
+
 from .api import open_rcm  # noqa: F401
 
 try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
-    import importlib_metadata as metadata
-__version__ = metadata.version("xarray-safe-rcm")
+    __version__ = version("xarray-safe-rcm")
+except Exception:
+    __version__ = "999"
