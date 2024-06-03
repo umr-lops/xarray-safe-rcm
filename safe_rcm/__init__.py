77cb519704b8923b1a0f6cf16c4dec146823d2cf
Antoine Grouazel <antoine.grouazel@ifremer.fr>
2024-06-03 11:23:08 +0200

    fix version number for safe_rcm (#83)
    * fix version number for safe_rcm
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * forget about the old python syntaxe, simply fix the package name
    
    * adopt fallback version 4 digits
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: agrouaze <antoine.grouazel@gmail.com>

--- safe_rcm/__init__.py
+++ safe_rcm/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import version
 from .api import open_rcm  # noqa: F401
 
 try:
-    __version__ = version("safe_rcm")
+    __version__ = version("xarray-safe-rcm")
 except Exception:
-    __version__ = "999"
+    __version__ = "9999"
